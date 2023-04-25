cf9e2284296c3d858eebe00285be832477210c6d
EyaG18 <eya.gadhoumi@esprit.tn>
2023-04-25 14:00:18 +0200

    commit2

--- Eya/view/deleteCinema.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../controller/cinemaC.php';
-$cinemaC=new cinemaC();
-$cinemaC->SupprimerCinema($_GET["IdCinema"]);
-header('location:showCinema.php');//bech nbqach nmchy f page vide (yaamel supprision w yarjaae directement ll page )
-?> 
--- Eya/view/deleteFilm.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../controller/filmC.php';
-$filmC=new filmC();
-$filmC->SupprimerFilm($_GET["IdFilm"]);
-header('location:showFilm.php');//bech nbqach nmchy f page vide (yaamel supprision w yarjaae directement ll page )
-?> 
